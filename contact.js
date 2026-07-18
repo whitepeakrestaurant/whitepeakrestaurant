4d2628329f4e813e052584612a7868d6cfdaaf40
whitepeakrestaurant <whitepeakrestaurant@gmail.com>
2026-07-18 15:31:37 +0500

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,18 @@
+// White Peak Restaurant Contact Form
+
+
+const form = document.querySelector(".message form");
+
+
+form.addEventListener("submit", function(){
+
+setTimeout(function(){
+
+alert(
+"Thank you for contacting White Peak Restaurant. Your message has been sent successfully!"
+);
+
+},500);
+
+
+});
